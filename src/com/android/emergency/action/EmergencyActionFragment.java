655121313a8ab14ba9025c2719034995363b4d64
lindatseng <lindatseng@google.com>
2020-10-12 23:16:05 -0700

    Add subtitle and countdown animation view to emergnecy action view
    CountDownAnimationView is mostly copied from
    jcgaa/safetyhub/common/widget/checkanimationview
    with small refactor to simplify the code
    for the usage in this screen only
    
    Screenshot:
    https://screenshot.googleplex.com/4GrjcRhRwGzhWFp.png
    
    Bug: 169946301
    Fix: 170431224
    Test: Tested on device
    Change-Id: Id8ee0149e30b7e3ebef801c78dd8fc536e7f0b01

--- src/com/android/emergency/action/EmergencyActionFragment.java
+++ src/com/android/emergency/action/EmergencyActionFragment.java
@@ -17,6 +17,7 @@
 package com.android.emergency.action;
 
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -24,16 +25,93 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.annotation.UiThread;
 
 import com.android.emergency.R;
+import com.android.emergency.widgets.countdown.CountDownAnimationView;
 
-@UiThread
+import java.time.Duration;
+
 public class EmergencyActionFragment extends Fragment {
+
+    private static final String STATE_MILLIS_LEFT = "STATE_MILLIS_LEFT";
+
+    private CountDownTimer mCountDownTimer;
+    private long mCountDownMillisLeft;
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater,
             @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.emergency_action_fragment, container, false);
+        View view = inflater.inflate(R.layout.emergency_action_fragment, container, false);
+
+        if (savedInstanceState != null) {
+            mCountDownMillisLeft = savedInstanceState.getLong(STATE_MILLIS_LEFT);
+        } else {
+            mCountDownMillisLeft =
+                    getResources().getInteger(R.integer.emergency_action_count_down_millis);
+        }
+
+        return view;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        startTimer();
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putLong(STATE_MILLIS_LEFT, mCountDownMillisLeft);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        if (mCountDownTimer != null) {
+            CountDownAnimationView countDownAnimationView =
+                    getView().findViewById(R.id.count_down_view);
+            countDownAnimationView.stop();
+            mCountDownTimer.cancel();
+        }
+    }
+
+    private void startTimer() {
+        CountDownAnimationView countDownAnimationView =
+                getView().findViewById(R.id.count_down_view);
+
+        if (mCountDownTimer != null) {
+            countDownAnimationView.stop();
+            mCountDownTimer.cancel();
+        }
+
+        mCountDownTimer =
+                new CountDownTimer(
+                        mCountDownMillisLeft,
+                        getResources().getInteger(R.integer.emergency_action_count_down_interval)) {
+                    @Override
+                    public void onTick(long millisUntilFinished) {
+                        CountDownAnimationView countDownAnimationView =
+                                getView().findViewById(R.id.count_down_view);
+                        if (countDownAnimationView != null) {
+                            countDownAnimationView.setCountDownLeft(
+                                    Duration.ofMillis(millisUntilFinished));
+                        }
+
+                        mCountDownMillisLeft = millisUntilFinished;
+                    }
+
+                    @Override
+                    public void onFinish() {
+                        getActivity().finish();
+                        // TODO(b/169946688): Call emergency service on count down finish.
+                    }
+                };
+
+        mCountDownTimer.start();
+
+        countDownAnimationView.start(Duration.ofMillis(mCountDownMillisLeft));
+        countDownAnimationView.showCountDown();
     }
 }
